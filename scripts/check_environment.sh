7d5f6d57f8befd05067f1abd8e8b9488391eaea9
Charles Shih <shichen120@gmail.com>
2019-09-09 15:07:54 +0800

    update check_environment.sh

--- scripts/check_environment.sh
+++ scripts/check_environment.sh
@@ -6,6 +6,7 @@
 #   v1.0  2017-11-23  charles.shih  init version
 #   v2.0  2019-09-04  charles.shih  Refactory
 #   v2.1  2019-09-05  charles.shih  Enhance the outputs
+#   v2.2  2019-09-06  charles.shih  Bugfix for rps display
 
 nif=eth0
 
@@ -17,23 +18,23 @@ res_np=$(netperf -V 2>/dev/null)
 res_i3=$(iperf3 -v 2>/dev/null | head -n1)
 : ${res_i3:="Not installed"}
 
+# check IRQ balance
+res_ir=$(systemctl is-active irqbalance)
+
 # check multiple queue
 res=$(ethtool -l $nif | grep "^Combined:")
 max=$(echo $res | cut -d ' ' -f 2)
 cur=$(echo $res | cut -d ' ' -f 4)
 res_nq="${cur:-?}/${max:-?}"
 
-# check IRQ balance
-res_ir=$(systemctl is-active irqbalance)
-
 # check rps
 res_rp=""
 for file in $(ls /sys/class/net/$nif/queues/rx-*/rps_cpus); do
-    res_rp=${res_rp}$(cat $file)
+    res_rp=${res_rp}:$(cat $file)
 done
 
 # Show summary
-echo "${res_np},${res_i3},${res_nq},${res_rp},${res_ir}" |
-    column -s "," -t --table-columns NETPERF,IPERF3,NIC_QUEUE,RPS_STAT,IRQ_BALANCE
+echo "${res_np};${res_i3};${res_ir};${res_nq};${res_rp}" |
+    column -s ";" -t --table-columns NETPERF,IPERF3,IRQ_BALANCE,NIC_QUEUE,RPS_STAT
 
 exit 0
