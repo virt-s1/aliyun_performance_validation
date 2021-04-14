22d5d944a87189ebad328aae9c34ec7f43871f5a
Charles Shih <schrht@gmail.com>
2021-04-14 17:47:42 +0800

    Add scripts and playbook for the testing.

---
+++ storage/scripts/summarize.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Owner: Charles Shih <schrht@gmail.com>
+# Description: Analyse fio json outputs and summarize the report
+
+function analyse() {
+    # $1: fio json log
+    file=$1
+    flavor=$(echo $file | cut -d '-' -f 2)
+    [ -z "$flavor" ] && flavor=unknown
+
+    rw=$(cat $file | jq -r '."global options".rw')
+    bs=$(cat $file | jq -r '."global options".bs')
+    iodepth=$(cat $file | jq -r '."global options".iodepth')
+    numjobs=$(cat $file | jq -r '."global options".numjobs')
+
+    iops_r=$(cat $file | jq -r '.jobs[0].read.iops')
+    iops_w=$(cat $file | jq -r '.jobs[0].write.iops')
+    iops=$(echo "$iops_r + $iops_w" | bc)
+
+    bw_r=$(cat $file | jq -r '.jobs[0].read.bw')
+    bw_w=$(cat $file | jq -r '.jobs[0].write.bw')
+    bw=$(echo "($bw_r + $bw_w) / 1024" | bc) # KiB/s to MiB/s
+
+    lat_ns_r=$(cat $file | jq -r '.jobs[0].read.lat_ns.mean')
+    lat_ns_w=$(cat $file | jq -r '.jobs[0].write.lat_ns.mean')
+    lat_ms=$(echo "($lat_ns_r + $lat_ns_w) / 1000000" | bc) # ns to ms
+
+    table="${table}$(printf '%s;%s;%s;%d;%d;%d;%d;%d;%s' \
+        $flavor $rw $bs $iodepth $numjobs $iops $bw $lat_ms $file)\n"
+}
+
+function print() {
+    echo -e $table | column -t -s ';' \
+        -N "Flavor,RW,BS,IODepth,Numjobs,IOPS,BW(MiB/s),Lat(ms),Logfile"
+}
+
+# Main
+logdir=$HOME/workspace/log
+cd $logdir || exit 1
+flavor=$(curl http://100.100.100.200/latest/meta-data/instance/instance-type)
+output=$logdir/summary-${flavor:-unknown}.log
+
+for log in $(ls fio-*.log); do
+    analyse $log
+done
+
+print | tee $output
+
+exit 0
---
+++ storage/scripts/test100w.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Owner: Charles Shih <schrht@gmail.com>
+# Description: Perform fio tests and collect the storage performance numbers
+#
+# Ref. https://www.alibabacloud.com/help/zh/doc-detail/65077.htm?spm=a2c63.p38356
+
+function RunFio() {
+    numjobs=$1  # 实例中的测试线程数，例如示例中的10
+    iodepth=$2  # 同时发出I/O数的上限，例如示例中的64
+    bs=$3       # 单次I/O的块文件大小，例如示例中的4k
+    rw=$4       # 测试时的读写策略，例如示例中的randwrite
+    filename=$5 # 指定测试文件的名称，例如示例中的/dev/vdb
+    nr_cpus=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+    if [ $nr_cpus -lt $numjobs ]; then
+        echo "Numjobs is more than cpu cores, exit!"
+        return 1
+    fi
+    let nu=$numjobs+1
+    cpulist=""
+    for ((i = 1; i < 10; i++)); do
+        list=$(cat /sys/block/vdb/mq/*/cpu_list |
+            awk '{if(i<=NF) print $i;}' i="$i" | tr -d ',' | tr '\n' ',')
+        if [ -z $list ]; then
+            break
+        fi
+        cpulist=${cpulist}${list}
+    done
+    spincpu=$(echo $cpulist | cut -d ',' -f 2-${nu})
+    echo "cpus_allowed=$spincpu"
+    fio --ioengine=libaio --runtime=30s --numjobs=${numjobs} \
+        --iodepth=${iodepth} --bs=${bs} --rw=${rw} --filename=${filename} \
+        --time_based=1 --direct=1 --name=test --group_reporting \
+        --cpus_allowed=$spincpu --cpus_allowed_policy=split \
+        --ramp_time=5 --output-format=json+ --output=/tmp/fio.log
+}
+
+# Main
+target=/dev/vdb
+logdir=$HOME/workspace/log
+mkdir -p $logdir
+flavor=$(curl http://100.100.100.200/latest/meta-data/instance/instance-type 2>/dev/null)
+[ -z "$flavor" ] && flavor=unknown
+timestamp=$(date +D%y%m%dT%H%M%S)
+
+# Check
+if [ ! -b $target ]; then
+    echo "Disk $target is not ready."
+    exit 1
+fi
+
+# Tuning
+echo 2 >/sys/block/vdb/queue/rq_affinity
+sleep 5
+
+# Test IOPS and BW
+RunFio 10 64 4k randwrite $target
+mv /tmp/fio.log $logdir/fio-$flavor-$timestamp-iops.log
+
+RunFio 10 64 1m write $target
+mv /tmp/fio.log $logdir/fio-$flavor-$timestamp-bw.log
+
+exit 0
