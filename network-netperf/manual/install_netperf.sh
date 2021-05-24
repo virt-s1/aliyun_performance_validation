095803d6a6999a78d5c50c8706e96495876cbbc8
Charles Shih <schrht@gmail.com>
2021-05-24 09:30:05 +0800

    Add new network testing.

---
+++ network-netperf/manual/install_netperf.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script is used in VM to install netperf for RHEL system.
+
+# History:
+#   v1.0  2017-11-23  charles.shih  init version
+#   v2.0  2019-09-04  charles.shih  Support RHEL8
+#   v2.1  2019-09-04  charles.shih  Minor bugfix
+
+type netperf && echo "Already installed." && exit 0
+
+cd $(mktemp -d)
+
+grep -q "release 8" /etc/redhat-release
+if [ $? -eq 0 ]; then
+	# on RHEL8
+	dnf install -y wget make gcc
+	wget https://github.com/HewlettPackard/netperf/archive/netperf-2.7.0.tar.gz
+	tar -xf netperf-2.7.0.tar.gz
+	cd netperf-netperf-2.7.0/
+	./configure && make && make install
+fi
+
+cat /etc/redhat-release | grep "release 7" &>/dev/null
+if [ $? -eq 0 ]; then
+	# on RHEL7
+	curl -O http://www.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
+	rpm -ivh netperf-*.rpm
+fi
+
+cat /etc/redhat-release | grep "release 6" &>/dev/null
+if [ $? -eq 0 ]; then
+	# on RHEL6
+	curl -O http://www.rpmfind.net/linux/dag/redhat/el6/en/x86_64/dag/RPMS/netperf-2.6.0-1.el6.rf.x86_64.rpm
+	rpm -ivh netperf-*.rpm
+fi
+
+netperf -V
+netserver -V
+
+exit 0
+
