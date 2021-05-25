77e23b5f53e5c95b51d4e0fc2de77f3551e631f8
Charles Shih <schrht@gmail.com>
2021-05-25 12:13:30 +0800

    Add logic to dump results and configure clients.

--- network-sockperf/scripts/master.sh
+++ network-sockperf/scripts/master.sh
@@ -9,7 +9,6 @@ function show_usage() {
     echo "   CLIENTS: The list of clients' IP address."
     echo "   TIMEOUT: The interval of the test (default=30)."
     echo "DUPLICATES: The duplicates of the test (default=1)."
-
 }
 
 while getopts :hc:t:d: ARGS; do
@@ -57,33 +56,54 @@ fi
 : ${duplicates:=1}
 
 NIC=eth0
+BINPATH=~/workspace
+LOGPATH=~/workspace/log
 
 # Main
-rm -f /tmp/sockperf.log.*
-mkdir -p /root/workspace/log
-
 hostip=$(ifconfig $NIC | grep -w inet | awk '{print $2}')
 flavor=$(curl http://100.100.100.200/latest/meta-data/instance/instance-type 2>/dev/null)
+cpu_core=$(cat /proc/cpuinfo | grep process | wc -l)
+timestamp=$(date +D%y%m%dT%H%M%S)
 
 echo "hostip=$hostip"
 echo "flavor=$flavor"
 echo "clients=$clients"
 echo "timeout=$timeout"
+echo "cpu_core=$cpu_core"
 echo "duplicates=$duplicates"
+echo "timestamp=$timestamp"
+
+logdir=$LOGPATH/sockperf_${flavor}_${timestamp}
+mkdir -p $logdir
 
 for client in $clients; do
-    echo "Starting test from client $client..."
-    log=/root/workspace/log/transmit_${flavor}_${client}.log
-    ssh root@$client "/root/workspace/transmit.sh -s $hostip \
-        -d $duplicates -t $(($timeout + 40)) &>$log" &
+    echo "Setup $client..."
+    scp $BINPATH/transmit.sh root@$client:/tmp/ || exit 1
 done
 
-# Start data collecting
-datafile=/root/workspace/log/master_${flavor}.sa
+client_num=0
+for client in $clients; do
+    client_num=$((client_num + 1))
+    echo "Starting test from client $client..."
+    log=$logdir/transmit_${flavor}_${client}.log
+    ssh root@$client "/tmp/transmit.sh -s $hostip \
+        -d $duplicates -t $(($timeout + 40))" &>$log &
+done
+
+# Start data collection
+sa_pps=$logdir/master_${flavor}_pps.sa
 sleep 20 # ramp time
-sar -A 1 $timeout -o $datafile &>/dev/null
+sar -A 1 $timeout -o $sa_pps &>/dev/null
 wait # waiting for clients
 
 # Analyse
-logfile=/root/workspace/log/master_${flavor}.log
-sar -n DEV -f $datafile | tee $logfile
+links=$(($client_num * $cpu_core * $duplicates))
+links_detail="(=$client_num*$cpu_core*$duplicates)"
+rxpckps=$(sar -n DEV -f $sa_pps | grep "Average.*$NIC" | awk '{print $3}')
+
+# Dump results
+logfile=$logdir/sockperf_${flavor}_${timestamp}.log
+printf "%-20s %-4s %-5s %-15s %-8s %-15s\n" \
+    Flavor CPU# Links "(=CLT*CPU*DUP)" Duration PPSrx >>$logfile
+printf "%-20s %-4s %-5s %-15s %-8s %-15s\n" \
+    $flavor $cpu_core $links "$links_detail" $timeout $rxpckps >>$logfile
