ce90a677a909091cfb3858bda5e23bdc4450c405
Charles Shih <schrht@gmail.com>
2021-05-27 17:24:57 +0800

    Update script to compose inventory.

--- network-sockperf/update_inventory.sh
+++ network-sockperf/update_inventory.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-tmpfile=$(mktemp)
-ansible-playbook ./show_instance_info.yml &>$tmpfile
-# Ex. "instances_facts.instances[0].public_ip_address": "39.105.175.9"
-grep public_ip_address $tmpfile | cut -d '"' -f 4 | tee ./inventory
-rm -f $tmpfile
+set -e
+
+yaml_vars=./ansible_vars.yml
+inventory=./inventory
+
+# Get facts
+tmpfolder=/tmp/ansible_update_inventory
+region_id=$(cat $yaml_vars | shyaml -q get-value global.alicloud_region)
+instance_name=$(cat $yaml_vars | shyaml -q get-value instance.instance_name)
+ansible localhost -m ali_instance_info \
+    -a "alicloud_region=$region_id name_prefix=$instance_name" \
+    --tree $tmpfolder &>$tmpfolder/ali_instance_info.log
+x=$(cat $tmpfolder/localhost)
+
+# Compose inventory
+tmpfile=$tmpfolder/inventory
+: >$tmpfile
+
+# test
+echo "[tests]" >>$tmpfile
+public_ip=$(echo $x | jq -r ".instances[0].public_ip_address")
+private_ip=$(echo $x | jq -r ".instances[0].private_ip_address")
+echo "test ansible_host=$public_ip private_ip=$private_ip" >>$tmpfile
+
+# peers
+echo "[peers]" >>$tmpfile
+for ((i = 1; i < 100; i++)); do
+    public_ip=$(echo $x | jq -r ".instances[$i].public_ip_address")
+    private_ip=$(echo $x | jq -r ".instances[$i].private_ip_address")
+    [ "$public_ip" = "null" ] && break
+    echo "peer${i} ansible_host=$public_ip private_ip=$private_ip" >>$tmpfile
+done
+
+# Deliver
+cp $tmpfile $inventory
